0ac3cd70ab870e7be5f9cae3f043af28937ba5fe
TharunRajeev <53303380+TharunRajeev@users.noreply.github.com>
2024-12-06 16:11:56 -0800

    modified next.config.js
    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["tharunrajeev.github.io"],
-  },
-  output: 'export', // Enables static site export
-  basePath: '/Pokedex', // Replace <repository-name> with your GitHub repo name
-  assetPrefix: '/Pokedex', // Ensures correct paths for assets
+  output: 'export',
+  distDir: 'dist',
 };
 
 module.exports = nextConfig;
